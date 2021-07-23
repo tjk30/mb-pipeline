c986ef614b12030af99b3c1aff36c7c33999fa1e
Brianna Petrone <brianna.petrone@gmail.com>
2021-07-23 13:39:50 -0400

    Automate detection of single read or paired-end

--- code/hardac/4_trim-primers.sh
+++ code/hardac/4_trim-primers.sh
@@ -21,35 +21,71 @@ mkdir 3_trimprimer
 # Read in primer sequences from file (in order: forward; reverse; reverse complement of forward; and reverse complement of reverse)
 { IFS= read -r fwd && IFS= read -r rev && IFS= read -r fwdrc && IFS= read -r revrc; } < 0_reference/primers.txt
 
-# Remove forward and reverse primer from paired-end reads
-for read1 in 2_*/*_R1.fastq.gz; do
-	read2=${read1%R1.fastq.gz}R2.fastq.gz # Find its matched read 2
-	# This pulls the sample name, here [primer]-30-pool or [primer]-NTC in order 
-	# to carry it over to the output filename
-	regex='([^_]*)(_.*)' # Match up to first underscore to get name
-	name=$([[ ${read1##*/} =~ $regex ]] && echo "${BASH_REMATCH[1]}") # 1st capture grp
-	suffix1=$([[ ${read1##*/} =~ $regex ]] && echo "${BASH_REMATCH[2]}") # 2nd capture grp
-	suffix2=$([[ ${read2##*/} =~ $regex ]] && echo "${BASH_REMATCH[2]}") # 2nd capture grp
-	
-	# Perform paired-end trimming with cutadapt:
-	# Here, we always want to trim the leading forward primer
-	# And if the read is short enough, we may read into the reverse primer, and want to trim that too.
-	# Parameter settings as follows
-	# Specifying a linked adapter with -a makes the adapters that are anchored  required, and the non-anchored
-	# adapters  optional. -g makes both adapters required
-	# -a: Trim adapter from forward read
-	# -A: Trim adapter from reverse read 
-	# -o: The output file
-	# -p: The short form of 'paired output', a second output file alongside the normal -o output
-	# --minimum-length: Set this parameter so cutadapt doesn't preserve empty reads (length 0) in output,
-	# which can't be handled by DADA2
+# Determine if analyzing one read or two
+# Is R1 missing? 
+if ! ls 2_filter/*_R1_* 1> /dev/null 2>&1; then
+	echo "R1 does not exist, analyzing R2 only"
+	for read2 in 2_filter/*_R2_*; do 
+		fullname=${read2#2_filter/}
+		name=${fullname//_[^.]*/} # Pull filename without MiniSeq-added info
 
-	cutadapt \
-	--minimum-length 1 --discard-untrimmed \
-	-a "^${fwd:0};e=0.15...$revrc;e=0.15" \
-	-A "^${rev:0};e=0.15...$fwdrc;e=0.15" \
-	-o 3_trimprimer/$name$suffix1 -p 3_trimprimer/$name$suffix2 \
-	$read1 $read2 \
-	> 3_trimprimer/$name.out
+		# Do single-end trimming:
+		# Always trim the leading forward primer, and if the read is short enough to read
+		# into the reverse primer, remove that as well.
+		#		
+		# Parameters:
+		# Specifying a linked adapter with -a makes the adapters that are anchored 
+		# required, and the non-anchored adapters  optional. 
+		# -a: Trim adapter from forward read
+		# -o: The output file
+		# --minimum-length: Set this parameter so cutadapt doesn't preserve empty reads 
+		# (length 0) in output, which can't be handled by DADA2
 
-done
+		cutadapt \
+		--minimum-length 1 --discard-untrimmed \
+		-a "^${rev:0};e=0.15...$fwdrc;e=0.15" \
+		-o 3_trimprimer/$fullname $read2 \
+		> 3_trimprimer/$name.out	
+	done
+# Is R2 missing?  
+elif ! ls 2_filter/*_R2_* 1> /dev/null 2>&1; then
+	echo "R2 does not exist, analyzing R1 only"
+	for read1 in 2_filter/*_R1_*; do 
+		fullname=${read1#2_filter/}
+		name=${fullname//_[^.]*/} # Pull filename without MiniSeq-added info
+
+		# Do single-end filtering:
+		# R1 must have the correct amplicon primer anchored at 5' end
+		cutadapt \
+		--minimum-length 1 --discard-untrimmed \
+		-a "^${rev:0};e=0.15...$fwdrc;e=0.15" \
+		-o 3_trimprimer/$fullname $read1 \
+		> 3_trimprimer/$name.out	
+	done
+else
+	# Remove forward and reverse primer from paired-end reads
+	for read1 in 2_*/*_R1.fastq.gz; do
+		read2=${read1%R1.fastq.gz}R2.fastq.gz # Find its matched read 2
+		regex='([^_]*)(_.*)' # Match up to first underscore to get name
+		name=$([[ ${read1##*/} =~ $regex ]] && echo "${BASH_REMATCH[1]}") # 1st capture grp
+		suffix1=$([[ ${read1##*/} =~ $regex ]] && echo "${BASH_REMATCH[2]}") # 2nd capture grp
+		suffix2=$([[ ${read2##*/} =~ $regex ]] && echo "${BASH_REMATCH[2]}") # 2nd capture grp
+		
+		# Perform paired-end trimming with cutadapt:
+		# -a: Trim adapter from forward read
+		# -A: Trim adapter from reverse read 
+		# -o: The output file
+		# -p: The short form of 'paired output', a second output file alongside the normal -o output
+		# --minimum-length: Minimum length of reads preserved by cutadapt
+		cutadapt \
+		--minimum-length 1 --discard-untrimmed \
+		-a "^${fwd:0};e=0.15...$revrc;e=0.15" \
+		-A "^${rev:0};e=0.15...$fwdrc;e=0.15" \
+		-o 3_trimprimer/$name$suffix1 -p 3_trimprimer/$name$suffix2 \
+		$read1 $read2 \
+		> 3_trimprimer/$name.out
+
+	done
+fi
+
+conda deactivate # Close conda environment
