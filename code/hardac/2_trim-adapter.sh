6436949cc5c26242885bfd0530a63d5bbaa55734
brianna petrone <brianna.petrone@gmail.com>
2022-06-16 10:57:42 -0400

    Update for DCC container, remove hard-coded email

--- code/hardac/2_trim-adapter.sh
+++ code/hardac/2_trim-adapter.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=2_trim-adapter
+#SBATCH --partition scavenger
 #SBATCH --mem=20000
 #SBATCH --out=reports/2_trim-adapter-%j.out
 #SBATCH --error=reports/2_trim-adapter-%j.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
-#SBATCH --mail-user=blp23@duke.edu
 
 # Usage: trim-adapter.sh /marker-dir
 # where marker-dir is the directory containing reads that all share the same
@@ -13,6 +13,7 @@
 # This trims off read-through into Illumina adapter at the 3' side of the read,
 # which can occur if the amplicon size is <150 bp.
 
+module load BBMap/38.63
 mkdir $1/1_trimadapter
 cd $1/0_raw_demux
 
@@ -21,7 +22,7 @@ for read1 in *R1_001.fastq.gz; do
 	read2=${read1%R1_001.fastq.gz}R2_001.fastq.gz
 
 	# Perform adapter trimming with BBDuk
-	~/bin/bbmap/bbduk.sh in1=$read1 in2=$read2 \
+	/opt/apps/rhel7/bbmap/bbduk.sh in1=$read1 in2=$read2 \
 	literal=CTGTCTCTTATACACATCT out1=../1_trimadapter/$read1 \
 	out2=../1_trimadapter/$read2 \
 	ktrim=r k=19 mink=11 hdist=3 tbo tpe \
