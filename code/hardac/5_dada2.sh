32c2051b13d01c1cc5995055872004fd273ab14b
brianna petrone <brianna.petrone@gmail.com>
2022-06-16 11:02:01 -0400

    Update for DCC, remove hard-coded email

--- code/hardac/5_dada2.sh
+++ code/hardac/5_dada2.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=5_dada2
+#SBATCH --partition scavenger
 #SBATCH --mem=64000
 #SBATCH -n 2  # Number of cores
 #SBATCH --out=reports/5_dada2-%j.out
 #SBATCH --error=reports/5_dada2-%j.err
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
-#SBATCH --mail-user=blp23@duke.edu
 
 # Usage: 5_dada2.sh /marker-dir input output,  
 # where marker-dir is the directory containing reads that all share the same 
 # primer set (can group together as dada2 infers variants) 
 
-cd /data/common/qiime2
-source miniconda/bin/activate qiime2-2019.10
+module load Anaconda3/5.1.0
+module load R/4.1.1-rhel8
+source activate /hpc/group/ldavidlab/modules/qiime2-env
 
 ## Make output folders (can't figure out how to do this in R script)
 # mkdir $1/$3 
