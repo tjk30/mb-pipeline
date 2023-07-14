3faf833ce0249a62fbb594a64f18e872a1a25402
tjk30 <83459346+tjk30@users.noreply.github.com>
2023-07-14 10:02:54 -0400

    Update 1_demux-barcode.sh
    moving trnL vs 12sv5 component to different script

--- code/1_demux-barcode.sh
+++ code/1_demux-barcode.sh
@@ -8,7 +8,7 @@
 #SBATCH --mail-type=END
 
 # Usage: 1_demux-barcode.sh /container-dir /miniseq-dir samplesheetname runType
-# Example: sbatch --mail-user=youremail@duke.edu 1_demux-barcode.sh /path/to/metabarcoding.sif path/to/miniseq-dir XXXXXXXX_sample-sheet.csv <trnL>OR<12SV5>
+# Example: sbatch --mail-user=youremail@duke.edu 1_demux-barcode.sh /path/to/metabarcoding.sif path/to/miniseq-dir XXXXXXXX_sample-sheet.csv 
 # This assumes that the sample sheet is located in the parent directory containing the MiniSeq results
 # folder
 codedir=$PWD
@@ -20,25 +20,6 @@ resFolder=$now'_results'
 outdir=$parent/$resFolder
 mkdir $outdir
 
-mkdir $outdir/0_reference
-
-if [[ "$4" = "trnL" ]]
-then
-echo "You have selected trnL as your run type"
-echo "GGGCAATCCTGAGCCAA 
-CCATTGAGTCTCTGCACCTATC
-TTGGCTCAGGATTGCCC
-GATAGGTGCAGAGACTCAATGG" >> $outdir/0_reference/primers.txt #these are the trnLGH primer sequences
-elif [[ "$4" = "12SV5" ]]
-then
-echo "You have selected 12SV5 as your run type"
-echo "TAGAACAGGCTCCTCTAG
-TTAGATACCCCACTATGC
-CTAGAGGAGCCTGTTCTA
-GCATAGTGGGGTATCTAA" >> $outdir/0_reference/primers.txt #these are 12SV5 primers
-else
-echo "ERROR: please enter exactly 'trnL' or '12SV5' as your run type"
-fi
 cd $2
 # Demultiplex
 singularity exec --bind $parent $1 bcl2fastq -o $outdir --interop-dir InterOp/$now --stats-dir Stats/$now --reports-dir Reports/$now --minimum-trimmed-read-length 0 --mask-short-adapter-reads 0 --sample-sheet $parent/$3
