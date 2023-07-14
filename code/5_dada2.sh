e26ad1c6b42de72c82c958f55de0ebb3c81d41d8
tjk30 <83459346+tjk30@users.noreply.github.com>
2023-07-14 10:13:26 -0400

    make flexible for 12s or trnL

--- code/5_dada2.sh
+++ code/5_dada2.sh
@@ -8,16 +8,16 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=END
 
-# Usage: 5_dada2.sh /marker-dir input output /container-dir
+# Usage: 5_dada2.sh /marker-dir runType /container-dir
 # where marker-dir is the directory containing reads that all share the same 
 # primer set (can group together as dada2 infers variants) 
-# Example: sbatch --mail-user=youremail@duke.edu 5_dada2.sh /path/to/XXXXXXXX_results 3_trimprimer 4_dada2output /path/to/metabarcoding.sif
+# Example: sbatch --mail-user=youremail@duke.edu 5_dada2.sh /path/to/XXXXXXXX_results trnL /path/to/metabarcoding.sif
 
-## Make output folders (can't figure out how to do this in R script)
-mkdir $1/$3 
+## Make output folders 
+mkdir $1/4_dada2output
 
 # Run dada2
-singularity exec --bind $1,$PWD $4 Rscript 5_Rscript-echo.R 5_dada2.R $1 $2 $3
+singularity exec --bind $1,$PWD $3 Rscript 5_Rscript-echo.R 5_dada2.R $1 $2 
 
 # cleanup
 mv 5_dada2.out $1/Reports
